349cd35a92883ff47a49ff05a3c28c449b4fb450
hayakawa <hykw77@gmail.com>
2016-09-27 23:45:00 +0900

    Chromedriver sample追加

---
+++ workspace/WebTestSample/src/com/example/ChromedriverSample.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromedriverSample {
+
+	@Test
+	public void testGoogleSearch() throws InterruptedException {
+	  // Optional, if not specified, WebDriver will search your path for chromedriver.
+	  System.setProperty("webdriver.chrome.driver", "F:/e46/chromedriver_win32/chromedriver.exe");
+
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("http://www.google.com/xhtml");
+	  Thread.sleep(5000);  // Let the user actually see something!
+	  WebElement searchBox = driver.findElement(By.name("q"));
+	  searchBox.sendKeys("ChromeDriver");
+	  searchBox.submit();
+	  Thread.sleep(5000);  // Let the user actually see something!
+	  driver.quit();
+	}
+
+
+
+
+}
